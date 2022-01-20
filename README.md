0840b323d70af12fd37939766abe0f519294b8e7
Aaditya Joshi <aadityajoshi151@gmail.com>
2022-01-20 18:24:39 +0530

    add gif, comments, space fix

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## pyinit
 
 Simple script which helps me create a folder structure and initialize git repo when starting a new python project.
+Feel free to fork it and customize it for your personal use.
+
+![Demo](assets/Demo.gif)
--- src/pyinit.py
+++ src/pyinit.py
@@ -3,28 +3,29 @@ import os
 print(os.getcwd())
 path = input("Please enter the path\n")
 project_name = input("Please enter project name\n")
-os.chdir(path)
-os.mkdir(project_name)
+project_name = project_name.replace(" ","_")  # Replaces spaces with underscores
+os.chdir(path)  #Changes current working directory to the path provided
+os.mkdir(project_name)  #Creates folder with project name
 path = path+"/"+project_name
-os.chdir(path)
-os.system("git init")
+os.chdir(path)  #Changes current working directory to the folder created
+os.system("git init")  #Initializes git repository
 os.mkdir("assets")
-print("Assets folder created")
+print("Assets folder created")  #Creates assets folder
 os.mkdir("src")
-print("src folder created")
-myfile = open("README.md","w")
-myfile.write(f"## {project_name}")
+print("src folder created")  #Creates src folder
+myfile = open("README.md","w")  #Creates README file
+myfile.write(f"## {project_name.replace('_',' ')}")  #Writes project name in h2 in README file. Spaces are allowed.
 myfile.close()
 print("README file created")
-myfile = open("requirements.txt","w")
+myfile = open("requirements.txt","w")  #Creates requirements file
 myfile.close()
 print("requirements file created")
-myfile = open(".gitignore","w")
+myfile = open(".gitignore","w")  #Creates gitignore file
 myfile.close()
 print("gitignore file created")
 path = path+"/src"
-os.chdir(path)
-myfile = open(f"{project_name}.py","w")
+os.chdir(path)  #Moves into the src folder
+myfile = open(f"{project_name}.py","w")  #Creates empty python file with project name
 myfile.close()
 print(f"{project_name}.py file created")
 print("All Done!")
